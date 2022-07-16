11be72c2cd087357d0a55af28af2484f5555302e
ag502 <ag502@naver.com>
2022-07-16 14:44:42 +0900

    add: asyncData 속성 추가

---
+++ components/ProductList.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    {{ products }}
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- pages/main.vue
+++ pages/main.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
     <p>메인 페이지 입니다</p>
+    <ProductList />
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import ProductList from '~/components/ProductList.vue'
 
 export default {
   name: 'MainPage',
 
-  async created() {
-    const response = await axios.get('http://localhost:8000/products')
-    console.log(response)
+  components: { ProductList },
+
+  async asyncData() {
+    const { data } = await axios.get('http://localhost:8000/products')
+    return { products: data }
   },
 }
 </script>

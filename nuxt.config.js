2b31add0a50759a59811f425fb36778ab51467a9
ag502 <ag502@naver.com>
2022-07-23 11:18:41 +0900

    add: css reset 파일 설정 추가

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   head: {
     title: 'learn-nuxt',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'kr',
     },
     meta: [
       { charset: 'utf-8' },
@@ -15,7 +15,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['@/assets/css/reset.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],

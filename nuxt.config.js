0aa7ae175076dcaa5bc86e11490edbbb58718361
ag502 <ag502@naver.com>
2022-07-31 12:49:37 +0900

    fix: description meta 태그 변경

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,19 @@
 export default {
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'learn-nuxt',
+    title: 'Nuxt Shopping',
     htmlAttrs: {
       lang: 'kr',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Nuxt Shopping example',
+      },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
